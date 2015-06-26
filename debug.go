d0a8ae223a5ecf83c2cba099241293cdba2fe6ea
Andrea Masi <eraclitux@gmail.com>
2015-06-26 18:22:23 +0200

    Add example

--- debug.go
+++ debug.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2014 Andrea Masi. All rights reserved.
+// Copyright (c) 2015 Andrea Masi. All rights reserved.
 // Use of this source code is governed by a MIT license
 // that can be found in the LICENSE.txt file.
 
--- release.go
+++ release.go
@@ -1,13 +1,18 @@
-// Copyright (c) 2014 Andrea Masi. All rights reserved.
+// Copyright (c) 2015 Andrea Masi. All rights reserved.
 // Use of this source code is governed by a MIT license
 // that can be found in the LICENSE.txt file.
 
 // +build !debug
 
 // Package stracer is possibly the simplest tracing package.
-// To enable its functions to print just build with ``debug`` tag otherwise they will do noop:
+// The idea is that during early stages of code development one wants
+// an easy way to inspect values of vars that can be quickly disabled.
+//
+// To enable stracer functions to print just build, run or test with ``debug`` tag otherwise they will do noop:
 // 	go build -tags debug
-// stderr is used to not perturb example functions.
+// 	go test -tags debug
+// 	go run -tags debug main.go
+// stderr is used to not perturb Examples() functions.
 //
 // Credits
 //
@@ -15,12 +20,12 @@
 package stracer
 
 // Traceln prints to stderr if '-tags debug'
-// is used when building, noop otherwise.
+// is used when building/running, noop otherwise.
 // stderr is used to not perturb example tests.
 func Traceln(args ...interface{}) {}
 
 // Tracef prints to stderr using supplied format
-// if '-tags debug' is used when building,
+// if '-tags debug' is used when building/running,
 // noop otherwise.
 // stderr is used to not perturb example tests.
 func Tracef(format string, a ...interface{}) {}
